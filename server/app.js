fe9350cb4d5c528697c77ef825e0959126efbf93
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-01-19 17:19:19 -0500

    5-3 install graphql

--- server/app.js
+++ server/app.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app=express();
+const {graphqlHTTP}=require("express-graphql");
+
+app.use('/graphql',graphqlHTTP({
+    graphiql:true
+}))
+
+
+
 
 app.listen(4000,()=>{console.log(`Listening on port 4000!`)});
