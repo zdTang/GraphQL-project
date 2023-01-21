38ab4846d1a7ed49d3859096912e8e53617cf395
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-01-21 11:01:44 -0500

    6-4 Create Hobby with mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -147,6 +147,23 @@ const Mutation = new GraphQLObjectType({
         return post;
       },
     },
+    // Create Hobby
+    CreateHobby: {
+      type: HobbyType,
+      args: {
+        title: { type: GraphQLString },
+        description: { type: GraphQLString },
+        userId: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        let hobby = {
+          title: args.title,
+          description: args.description,
+          userId: args.userId,
+        };
+        return hobby;
+      },
+    },
   },
 });
 
