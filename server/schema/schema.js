629fc5dc246a634f29398990a84b60d59c2d22fc
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-01-21 11:24:22 -0500

    6-6 Query Posts and Hobbies

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -107,11 +107,23 @@ const RootQuery = new GraphQLObjectType({
         return _.find(hobbiesData, { id: args.id });
       },
     },
+    hobbies: {
+      type: new GraphQLList(HobbyType),
+      resolve(parent, args) {
+        return hobbiesData;
+      },
+    },
     post: {
       type: PostType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return _.find(postsData, { id: args.id });
+      },
+    },
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve(parent, args) {
+        return postsData;
       },
     },
   },
