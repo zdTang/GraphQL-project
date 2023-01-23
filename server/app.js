965550f667105ee31ed1b76a548ce26a3dec7723
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-01-23 09:30:33 -0500

    8-3 setting up Project to Connect MongoDB Atlas

--- server/app.js
+++ server/app.js
@@ -2,18 +2,30 @@ const express = require("express");
 const app = express();
 const expressQraphQL = require("express-graphql");
 const { graphqlHTTP } = expressQraphQL;
+const process = require("./nodemon.json");
 
 const schema = require("./schema/schema");
 const schema_test = require("./schema/types_schema");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false); // based on warning
+const port = process.env.PORT || 4000;
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@cluster0.y6term8.mongodb.net/${process.env.mongoDatabase}`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    app.listen({ port: port }, () => {
+      console.log(`Listening on port ${port}!`);
+    });
+  })
+  .catch((error) => console.dir(error));
 
 app.use(
   "/graphql",
   graphqlHTTP({
     graphiql: true,
-    schema:schema_test,
+    //schema: schema_test,
+    schema: schema,
   })
 );
-
-app.listen(4000, () => {
-  console.log(`Listening on port 4000!`);
-});
